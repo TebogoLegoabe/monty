2b4159f458a500005550eb8b8295bd47861feca4
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-05-25 16:16:50 -0700

    do nothing

--- nop.c
+++ nop.c
@@ -5,8 +5,10 @@
   * @head: Pointer to the head of the stack
   * @null: Unsigned integer parameter (unused)
   *
-  * Description: This function is a no-operation instruction (nop) in the Monty language.
-  *              It does not perform any operations on the stack or modify any data.
+  * Description: This function is a no-operation instruction
+  * (nop) in the Monty language.
+  *              It does not perform any operations on
+  *              the stack or modify any data.
   * Return: void
   */
 void nop(stack_t **head, unsigned int null)
